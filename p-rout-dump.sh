693de05edf392925a1ed912d5cd3d68f0e2cb1eb
Bert Burgemeister <trebbu@googlemail.com>
2014-06-16 22:00:30 +0200

    Dump database daily

--- p-rout-dump.sh
+++ p-rout-dump.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+
+dumpdir=${1-'/var/lib/p-rout/'}
 filename=dump$(date +%Y%m%d-%H%M).sql.gz
-pg_dump -Z 9 -U p-rout -f $filename -w p_rout
 
-
+find $dumpdir/ -name dump*-*.sql.gz -mtime +14 -delete
+pg_dump -Z 9 -U p-rout -f $dumpdir/$filename -w p_rout
